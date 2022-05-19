bf5b7c893af181954683c09fbb18163ac900b3bd
ak-alam <akbar.alam@cheetay.pk>
2022-05-20 00:07:48 +0500

    updated trigger file and added route in app file

--- app.py
+++ app.py
@@ -6,6 +6,9 @@ app = Flask(__name__)
 def welcome():
     return "Flask Server Running"
     
-    
+@app.route('/hello', methods=['GET', 'POST'])
+def welcome():
+    return "Hello World!!"
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
