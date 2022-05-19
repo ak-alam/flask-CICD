e8778a9a2576bb671d101f70c357ca003fa95cd4
Akbar Alam <32440106+akbar-alam@users.noreply.github.com>
2022-05-19 23:34:37 +0500

    Update trigger.sh

--- trigger.sh
+++ trigger.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 chmod 400 id_rsa_flask.pem
-git config --global user.email "alam.akbar1989@gmail.com"
-git config --global user.name "akbar-alam"
+#git config --global user.email "alam.akbar1989@gmail.com"
+#git config --global user.name "akbar-alam"
 git checkout main
 date >> trigger
 git add .
 git commit -m "flask-cicd-build"
-git remote set-url origin git@github.com:akbar-alam/flask-CICD.git
+#git remote set-url origin git@github.com:akbar-alam/flask-CICD.git
 GIT_SSH_COMMAND="ssh -i id_rsa_flask.pem" git push origin main
