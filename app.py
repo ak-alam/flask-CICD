8ccf7db226d3cdff43f5249f2dfef2d0d3318893
ak-alam <akbar.alam@cheetay.pk>
2022-05-21 19:08:46 +0500

    added update script and changes in app file for testing

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ def welcome():
     
 @app.route('/hello', methods=['GET', 'POST'])
 def hello_world():
-    return "Hello World!!"
+    return "Hello World New deployment on ECS!!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
