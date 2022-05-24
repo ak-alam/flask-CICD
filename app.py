6b6b35bbd0f8ccba920581ef3af8f019f03cc21f
ak-alam <akbar.alam@cheetay.pk>
2022-05-24 14:02:16 +0500

    fix

--- app.py
+++ app.py
@@ -4,11 +4,11 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def welcome():
-    return "Flask Server Running"
+    return "Flask Server New Deployment"
     
 @app.route('/hello', methods=['GET', 'POST'])
 def hello_world():
-    return "Hello World New deployment on ECS!!"
+    return "Hello From AWS ECS!!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
